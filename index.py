46492d737948a526425639ffb8073e82d1132c17
Saki Yamauchi <saki.yamauchi6q@gmail.com>
2020-09-20 14:48:24 +0900

    add mongodb

--- index.py
+++ index.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+import datetime, pprint
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!!!"
+client = MongoClient('localhost', 27017)
+db = client.test_database
+collection = db.test_collection
+
+@app.route("/hello")
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/insert', methods=['GET', 'POST'])
+def insert():
+    post = {"author": "Mike", "text": "My first blog post!"}
+    post_id = collection.insert_one(post).inserted_id
+    return str(post_id)
+
+@app.route('/find_one', methods=['GET'])
+def find_one():
+    pprint.pprint(collection.find_one())
+    return str(collection.find_one())
---
+++ scripts/run-mongo.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run --rm -d -p 27017:27017 mongo:latest
